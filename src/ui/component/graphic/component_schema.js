49cbd2a4b0b80c0e98cd2ba23b987e3f83f875c0
Jarek Kubos <jarek@kubos.cz>
2016-12-07 22:38:23 +0100

    Disabled text selection

--- src/ui/component/graphic/component_schema.js
+++ src/ui/component/graphic/component_schema.js
@@ -34,9 +34,10 @@ export default class ComponentSchema extends React.Component {
     this.instances_geometry.update(instancePov.definition(), this.props.width, this.props.height)
 
     return <svg
-        style={{border: '1px solid gray', display: 'inline', }}
+        style={{border: '1px solid gray', display: 'inline', userSelect: 'none'}}
         width={this.props.width}
         height={this.props.height}
+
       >
 
       <rect
